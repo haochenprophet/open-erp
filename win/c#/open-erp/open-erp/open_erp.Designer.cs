9f4ebd69fa6cbab0b2e60362c5a89514d205c96b
chenhaohaochenlulu <xhfch@126.com>
2018-07-22 14:28:13 +0800

    Add win/c#/open-erp/

---
+++ win/c#/open-erp/open-erp/open_erp.Designer.cs
@@ -0,0 +1,20 @@
+﻿namespace open_erp
+{
+    partial class open_erp
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ win/c#/open-erp/open-erp/open_erp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace open_erp
+{
+    public partial class open_erp : Form
+    {
+        public open_erp()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 464);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(826, 22);
+            this.statusStrip.TabIndex = 0;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(826, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip";
+            // 
+            // open_erp
+            // 
+            this.ClientSize = new System.Drawing.Size(826, 486);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "open_erp";
+            this.Text = "open_erp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
